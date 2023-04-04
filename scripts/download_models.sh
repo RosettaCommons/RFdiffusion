487c72068a94d5a3fc10aa19093e1199f3f14261
Brian Loyal <bloyal@amazon.com>
2023-04-04 12:52:25 -0500

    Add script to download model files

---
+++ scripts/download_models.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Usage: bash download_models.sh /path/to/download/directory
+set -e
+
+if [[ $# -eq 0 ]]; then
+    echo "Error: download directory must be provided as an input argument."
+    exit 1
+fi
+
+DOWNLOAD_DIR="$1"
+
+if ! command -v wget &> /dev/null
+then
+    echo "Error: wget could not be found. Please install wget (sudo apt-get install wget)"
+    exit 1
+fi
+
+mkdir --parents "${DOWNLOAD_DIR}"
+wget -P "${DOWNLOAD_DIR}" \
+        http://files.ipd.uw.edu/pub/RFdiffusion/6f5902ac237024bdd0c176cb93063dc4/Base_ckpt.pt \
+        http://files.ipd.uw.edu/pub/RFdiffusion/e29311f6f1bf1af907f9ef9f44b8328b/Complex_base_ckpt.pt \
+        http://files.ipd.uw.edu/pub/RFdiffusion/60f09a193fb5e5ccdc4980417708dbab/Complex_Fold_base_ckpt.pt \
+        http://files.ipd.uw.edu/pub/RFdiffusion/74f51cfb8b440f50d70878e05361d8f0/InpaintSeq_ckpt.pt \
+        http://files.ipd.uw.edu/pub/RFdiffusion/76d00716416567174cdb7ca96e208296/InpaintSeq_Fold_ckpt.pt \
+        http://files.ipd.uw.edu/pub/RFdiffusion/5532d2e1f3a4738decd58b19d633b3c3/ActiveSite_ckpt.pt \
+        http://files.ipd.uw.edu/pub/RFdiffusion/12fc204edeae5b57713c5ad7dcb97d39/Base_epoch8_ckpt.pt
